ca034ecd909bf9423efe61f3a11f4822ca9462ab
rhewryk <78235971+rhewryk@users.noreply.github.com>
2021-11-27 12:29:21 -0500

    89

---
+++ Poster_Response/script.js
@@ -0,0 +1,4 @@
+
+const scaleSize = $(window).height() / $("#page").height(); //THIS CALCULATES THE SIZE OF THE PAGE relative to the size of the window
+
+ //$('#page').css({'transform':'scale('+scaleSize+')'})
